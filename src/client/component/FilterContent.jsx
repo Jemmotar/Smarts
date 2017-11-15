081e24356069a9ca4d5ed8abf1f0ae8a22bdf92c
Filipsi <cz.filipsi@gmail.com>
2017-11-15 22:34:34 +0100

    Moved stage selection logic to it's own component

--- src/client/component/FilterContent.jsx
+++ src/client/component/FilterContent.jsx
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
-import { Grid, Menu } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
+import StageMenu from './StageMenu.jsx';
 import FilterLoader from '~/src/filter/FilterLoader.js';
 
 export default class FilterContent extends Component {
-	constructor (props) {
-		super(props);
-
-		this.state = {
-			activeStage: undefined
-		};
-
-		this.handleItemClick = (e, { name }) => this.selectStage(name);
-	}
-
-	selectStage (name) {
-		this.setState({
-			activeStage: name
-		});
-	}
-
 	render () {
 		const { filter } = this.props;
-		const { activeStage } = this.state;
 
 		return (
 			<Grid>
 				<Grid.Column width={4}>
-					<Menu fluid vertical tabular>
-						{filter && FilterLoader.get(filter).stages.map((stage, index) => (
-							<Menu.Item key={index} name={stage.target} active={activeStage === stage.target} onClick={this.handleItemClick} />
-						))}
-					</Menu>
+					<StageMenu filter={filter} />
 				</Grid.Column>
 
 				<Grid.Column stretched width={12}>
---
+++ src/client/component/StageMenu.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import FilterLoader from '~/src/filter/FilterLoader.js';
+
+export default class StageMenu extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			activeStage: 0
+		};
+
+		this.handleStageSelection = (e) => this.selectStage(parseInt(e.target.attributes['data-index'].value));
+	}
+
+	selectStage (index) {
+		this.setState({
+			activeStage: index
+		});
+	}
+
+	render () {
+		const { filter } = this.props;
+		const { activeStage } = this.state;
+
+		return (
+			<Menu fluid vertical tabular>
+				{filter && FilterLoader.get(filter).stages.map((stage, index) => (
+					<Menu.Item key={index} data-index={index} active={activeStage === index} name={stage.target} onClick={this.handleStageSelection} />
+				))}
+			</Menu>
+		);
+	}
+}
